699722d76998d967a803f3f7472037fdd79c8787
Yunir Salimzyanov <yunir.me@gmail.com>
2019-12-01 23:48:21 +0300

    add two commands that publish monitoring messages every second

--- cmd/monitor.go
+++ cmd/monitor.go
@@ -3,15 +3,16 @@ package main
 import (
 	"github.com/nats-io/nats.go"
 	"log"
+	"strings"
 	"time"
+	"os/exec"
 )
 
 func main() {
+	var subject = "monitor.measure_temp"
 	var server_ip = "0.0.0.0:4222"
-	var subject = "monitor.data"
-	var payload = "I am there, come to my voice!"
 
-	opts := []nats.Option{nats.Name("Monitor")}
+	opts := []nats.Option{nats.Name("monitor")}
 
 	// Initialise NATS-client
 	nc, err := nats.Connect(server_ip, opts...)
@@ -20,16 +21,25 @@ func main() {
 	}
 	defer nc.Close()
 	for {
-		nc.Publish(subject, []byte(payload))
-		nc.Flush()
+		monitor_app := "/opt/vc/bin/vcgencmd"
+		monitor_cmds := [2]string{"measure_volts", "measure_temp"}
+		for _, m_cmd := range monitor_cmds {
+			sh_cmd := exec.Command(monitor_app, m_cmd)
+			stdout, _ := sh_cmd.Output()
+			stdout = []byte(strings.TrimSuffix(string(stdout), "\n"))
 
-		if err := nc.LastError(); err != nil {
-			//TODO: catch error if NATS.server is unreachable
-			log.Fatal(err)
-		} else {
-			log.Printf("Published [%s] : '%s'\n", subject, payload)
+			nc.Publish(subject, []byte(stdout))
+			nc.Flush()
+
+			if err := nc.LastError(); err != nil {
+				log.Fatal(err)
+			} else {
+				log.Printf("Published [%s] : '%s'\n", subject, stdout)
+			}
 		}
-		time.Sleep(5 * time.Second)
+
+
+		time.Sleep(1 * time.Second)
 	}
 }
 
